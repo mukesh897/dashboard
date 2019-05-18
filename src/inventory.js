6a767bfb1a0e34d7f82e65bb4cb533e77123db25
mukesh <mukesh.bhatia85@gmail.com>
2019-05-18 20:44:21 +0530

    Add table in centre component lower card

--- src/inventory.js
+++ src/inventory.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import logo from './assets/logo_mark.svg';
 
+import App from './App.css'
+
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -28,8 +30,21 @@ import Icon from '@material-ui/core/Icon';
 import DeleteIcon from '@material-ui/icons/Delete';
 import NavigationIcon from '@material-ui/icons/Navigation';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
 
 export default class Inventory extends React.Component{
+
+
+
   render(){
     return(
       <div>
@@ -39,33 +54,78 @@ export default class Inventory extends React.Component{
       </div>
 
       <div>
-        <Card >
-        <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <Badge badgeContent color="primary">
-              <Fab variant="extended" aria-label="Delete">
-                <NavigationIcon />
-                3 days to go
-              </Fab>
-            </Badge>
-          }
-          title=<h2>Anniversary celebrations</h2>
-          subheader=<div>Being the savage's bowsman,
-          that is, the person who pulled the bow-oar in his boat
-          (the second one from forward), it was my cheerful
-          duty to attend upon him while taking that hard- scrabble scramble
-          upon the dead whale's back.
-          You have seen Italian organ-boys holding a dancing-ape by a long cord.
-          <a href="/">Learn more</a></div>
-        />
-      </Card>
+        <Paper className="root">
+      <Table className="table">
+        <TableHead>
+          <TableRow>
+            <CustomTableCell>Dessert (100g serving)</CustomTableCell>
+            <CustomTableCell align="right">Calories</CustomTableCell>
+            <CustomTableCell align="right">Fat (g)</CustomTableCell>
+            <CustomTableCell align="right">Carbs (g)</CustomTableCell>
+            <CustomTableCell align="right">Protein (g)</CustomTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow className="row" key={row.id}>
+              <CustomTableCell component="th" scope="row">
+                {row.name}
+              </CustomTableCell>
+              <CustomTableCell align="right">{row.calories}</CustomTableCell>
+              <CustomTableCell align="right">{row.fat}</CustomTableCell>
+              <CustomTableCell align="right">{row.carbs}</CustomTableCell>
+              <CustomTableCell align="right">{row.protein}</CustomTableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
       </div>
     </div>
 )
 }
+}
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+});
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function CustomizedTable(props) {
+  const { classes } = props;
 }
